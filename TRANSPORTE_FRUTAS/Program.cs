3d2ca76bf8d833668c708887be9ad881369eedf6
Juan Paris <juanpajares6@gmail.com>
2023-09-05 21:55:05 -0500

    Agregar archivos de proyecto.

---
+++ TRANSPORTE_FRUTAS/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRANSPORTE_FRUTAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String tipoFruta;
+            int distanciaKilometros;
+            int costoTotal;
+            int costoCaja;
+            Console.WriteLine("Ingrese el tipo de fruta (Naranja, Fresa o Mango): ");
+             tipoFruta = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la distancia en kilómetros: ");
+             distanciaKilometros = int.Parse(Console.ReadLine());
+            switch (tipoFruta)
+            {
+                case "Naranja":
+                    if (distanciaKilometros < 30)
+                    {
+                        costoCaja = 2;
+                    }
+                    else
+                    {
+                        costoCaja = 4;
+                    }
+                    break;
+                case "Fresa":
+                    if (distanciaKilometros < 30)
+                    {
+                        costoCaja = 6;
+                    }
+                    else
+                    {
+                        costoCaja = 9;
+                    }
+                    break;
+                case "Mango":
+                    if (distanciaKilometros < 30)
+                    {
+                        costoCaja = 3;
+                    }
+                    else
+                    {
+                        costoCaja = 5;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Tipo de fruta no válido.");
+                    return ;
+            }
+
+            Console.WriteLine($"El costo por caja de frutas es: S/{costoCaja}");
+            Console.ReadKey();
+    
+
+        }    
+    } 
+}
